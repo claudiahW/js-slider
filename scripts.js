fc22ea38d900a50cb3c47e7e305d32b5d7a44dbc
claudiahW <murungiclaud@gmail.com>
2024-01-27 19:23:30 +0300

    iterate over slides object using an index

--- scripts.js
+++ scripts.js
@@ -3,3 +3,8 @@
 const slides = document.querySelectorAll(".slide");
 const nextBtn = document.querySelector(".nextBtn");
 const prevBtn = document.querySelector(".prevBtn");
+
+// iterate over slides object using foreach method using an index
+slides.forEach(function(slide,index){
+    slide.style.left = `${index * 100}%`
+})
