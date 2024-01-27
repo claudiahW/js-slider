d2bcd277c4ce5b46ceaa85ca0ea8882a68a29421
claudiahW <murungiclaud@gmail.com>
2024-01-27 19:47:14 +0300

    set up counter to listen to click events for prev and next buttons

--- scripts.js
+++ scripts.js
@@ -7,4 +7,21 @@ const prevBtn = document.querySelector(".prevBtn");
 // iterate over slides object using foreach method using an index
 slides.forEach(function(slide,index){
     slide.style.left = `${index * 100}%`
-})
+});
+
+// set up functionality to control which slide is shown and which one is hidden
+
+// this first function controls whether the button will navigate to next slide or will go back to start
+
+// set up counter for prev and next btn 
+let counter = 0;
+
+nextBtn.addEventListener("click",function(){
+ counter++;
+});
+
+prevBtn.addEventListener("click",function(){
+    counter--;
+   });
+
+   
